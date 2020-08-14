08fda24a50b141f6ba70e9b518ac2c6dc500e332
IgoVeyner <igorveyner95@gmail.com>
2020-08-14 19:23:26 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,8 +62,8 @@ class Dog
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      self
     end
+    self
   end
 
   def self.create(hash)
