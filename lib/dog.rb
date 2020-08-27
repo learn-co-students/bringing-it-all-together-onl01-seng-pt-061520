59c75965183acfcc11ab5323871b50630a5c11d0
drewsview34 <drewsview34@outlook.com>
2020-08-26 22:08:43 -0700

    Created the create table and drop table Class methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,29 @@ class Dog
     attr_accessor :name, :breed
     attr_reader :id
 
-    def initialize(id = nil, name:, breed:)
+    def initialize(id: nil, name: name, breed: breed)
+        @id = id
         @name = name
         @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+            DROP TABLE dogs
+        SQL
+
+        DB[:conn].execute(sql)
     end
 end
