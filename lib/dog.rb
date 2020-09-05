18f574529d27ffb42398641c21a6dcfdef67f429
Cameron Hudson <camnn3@yahoo.com>
2020-09-05 13:53:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,5 +76,9 @@ def self.find_of_create_by(name:, breed)
   WHERE name = ? AND breed = ?", name, breed)
   if !dog.empty?
     dog_data = dog[0]
-    dog = Dog.new(id: dog_date[0])
+    dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+  else
+    dog = self.create(name: name, breed: breed)
+  end
+  dog
 end
