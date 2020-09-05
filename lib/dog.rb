c7c9a96b1e9db8a3470a6e676a61d50949e95979
Cameron Hudson <camnn3@yahoo.com>
2020-09-05 13:53:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,5 +72,9 @@ end
 end
 
 def self.find_of_create_by(name:, breed)
-  
+  dog = DB[:conn].execute("SELECT * FROM dogs
+  WHERE name = ? AND breed = ?", name, breed)
+  if !dog.empty?
+    dog_data = dog[0]
+    dog = Dog.new(id: dog_date[0])
 end
